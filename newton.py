0de73f2cb4d5d154b8b50a288fe7cb04f2acc7d5
Andy Gu <ajgu@berkeley.edu>
2026-08-19 10:28:43 -0700

    added newton.py

---
+++ newton.py
@@ -0,0 +1,15 @@
+import math
+
+def derivative(x,f):
+    epsilon = 0.001
+    return (f(x+epsilon)-f(x))/epsilon
+
+def optimize(x, f):
+    epsilon = 0.001
+    y=x+2
+    d=2
+    while d>0.001:
+        d=derivative(x,f)/(derivative(x+epsilon,f)-derivative(x,f))*epsilon
+        y=x-d
+        x=y
+    return y,f(y)
