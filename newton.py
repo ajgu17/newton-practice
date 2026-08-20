17d033b2ee841f8561dd1788136d0232f15164c0
Andy Gu <ajgu@berkeley.edu>
2026-08-20 09:12:36 -0700

    fixed optimize

--- newton.py
+++ newton.py
@@ -1,14 +1,14 @@
 
 def derivative(x,f):
     """Compute derivative using finite different with epsilon = 0.001 of f at x"""
-    epsilon = 0.001
+    epsilon = 0.0001
     return (f(x+epsilon)-f(x))/epsilon
 
 def optimize(x, f):
     """Find the minimum of f starting from x"""
-    epsilon = 0.001
+    epsilon = 0.0001
     d=2
-    while d>0.001:
+    while abs(d)>0.001:
         d=derivative(x,f)/(derivative(x+epsilon,f)-derivative(x,f))*epsilon
         y=x-d
         x=y
